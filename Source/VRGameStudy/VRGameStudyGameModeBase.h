e0cf82aa98cff45cf2de2573bffa40209869554d
PiangGG <974805475@qq.com>
2021-07-19 17:28:55 +0800

    first commit

---
+++ Source/VRGameStudy/VRGameStudyGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VRGameStudyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VRGAMESTUDY_API AVRGameStudyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
